07ad2aa596aa109abe166d21a7ccb4a5bb0fa91c
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2022-09-02 09:16:45 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,18 +7,18 @@ export default function HomePage() {
     <>
       <div className={styles.container}>
         <div className={styles.background}>
-          <h1>I BUILD</h1>
-          <h1>WEBSITES</h1>
+          <h1>Full Stack</h1>
+          <h1>Developer</h1>
         </div>
         <div className={styles.foreground}>
           <div className={styles.content}>
-            <h1 className={styles.name}>Nitin Ranganath</h1>
+            <h1 className={styles.name}>Lucas Godoy</h1>
             <h6 className={styles.bio}>Full Stack Web Developer</h6>
             <Link href="/projects">
-              <button className={styles.button}>View Work</button>
+              <button className={styles.button}>Meus Projetos</button>
             </Link>
             <Link href="/contact">
-              <button className={styles.outlined}>Contact Me</button>
+              <button className={styles.outlined}>Entre em Contato</button>
             </Link>
           </div>
           <Illustration className={styles.illustration} />
