28a6d91100eefcadd4f438110ab72aa165232c9c
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2022-09-06 19:44:49 -0400

    Update github.jsx

--- pages/github.jsx
+++ pages/github.jsx
@@ -4,17 +4,9 @@ import RepoCard from '../components/RepoCard';
 import styles from '../styles/GithubPage.module.css';
 
 const GithubPage = ({ repos, user }) => {
-  const theme = {
-    level0: '#161B22',
-    level1: '#0e4429',
-    level2: '#006d32',
-    level3: '#26a641',
-    level4: '#39d353',
-  };
-
   return (
     <>
-    
+
     </>
   );
 };
