8dd0e33ca6872b9a28ec26dcbd3ae87b3d509dbd
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2022-09-06 21:25:07 -0400

    Update Head.jsx

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,20 +6,15 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="
+        Lucas Godoy é um ávido desenvolvedor web full stack que cria sites e aplicativos que você adoraria usar."
       />
-      <meta
-        name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
-      />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Portfólio de Lucas Godoy" />
       <meta
         property="og:description"
-        content="A full-stack developer building websites that you'd like to use."
+        content="Portfólio de Lucas Godoy, desenvolvedor web Full Stack."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
       <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
-      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
